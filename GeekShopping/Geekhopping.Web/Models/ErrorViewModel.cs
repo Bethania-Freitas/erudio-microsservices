052505d00bef70b8af016bf1c5a94d789a85c0ba
Bethânia Alves de Oliveira e Freitas <bethaniafreitas@yahoo.com.br>
2024-01-31 14:40:49 -0300

    iniciando projeto microsserviços

---
+++ GeekShopping/Geekhopping.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Geekhopping.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
