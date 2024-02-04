cc3aa76cdd4e89c3882cfe184fe60756cb3cc5eb
Bethânia Alves de Oliveira e Freitas <bethaniafreitas@yahoo.com.br>
2024-02-04 10:11:12 -0300

    Definindo Propriedades de interface e IproductRepository

---
+++ GeekShopping/GeekShopping.ProductAPI/Data/ValueObjects/ProductVO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GeekShopping.ProductAPI.Data.ValueObjects
+{
+    public class ProductVO
+    {
+        public long Id { get; set; }
+
+        public String Name { get; set; }
+
+        public string Price { get; set; }
+
+        public String Description { get; set; }
+
+        public String CategoryName { get; set; }
+
+        public String ImageURL { get; set; }
+    }
+}
