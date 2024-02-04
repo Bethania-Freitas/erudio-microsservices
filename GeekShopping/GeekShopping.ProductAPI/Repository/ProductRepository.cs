945b5c855c0d6b9957e31a396a223196f80d885f
Bethânia Alves de Oliveira e Freitas <bethaniafreitas@yahoo.com.br>
2024-02-04 10:36:05 -0300

    Implementacao do repositorio do produtos

---
+++ GeekShopping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model;
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly MySQLContext _context;
+        private IMapper _mapper;
+
+        public ProductRepository(MySQLContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<ProductVO>> FindAll()
+        {
+            List<Product> products = await _context.Products.ToListAsync();
+            return _mapper.Map<List<ProductVO>>(products);
+        }
+
+        public async Task<ProductVO> FindById(long id)
+        {
+            Product products = 
+                await _context.Products.Where(p => p.Id == id)
+                .FirstOrDefaultAsync();
+            return _mapper.Map < ProductVO > (products);
+        }
+        public Task<ProductVO> Create(ProductVO vo)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ProductVO> Update(ProductVO vo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
