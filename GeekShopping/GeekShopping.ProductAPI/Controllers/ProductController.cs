d4166c7906f74962dc1f2e42a53fd2170405490f
Bethânia Alves de Oliveira e Freitas <bethaniafreitas@yahoo.com.br>
2024-02-05 11:04:44 -0300

    Implementando Post, Put, Delete de produtos

--- GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -29,5 +29,29 @@ namespace GeekShopping.ProductAPI.Controllers
             if (product == null) return NotFound();
             return Ok(product);
         }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create(ProductVO vo)
+        {
+            if (vo == null) return BadRequest();
+            var product = await _repository.Create(vo);
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
+        {
+            if (vo == null) return BadRequest();
+            var product = await _repository.Update(vo);
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ProductVO>> Delete(long id)
+        {
+            var status = await _repository.Delete(id);
+            if (!status) return BadRequest();
+            return Ok(status);
+        }
     }
 }
