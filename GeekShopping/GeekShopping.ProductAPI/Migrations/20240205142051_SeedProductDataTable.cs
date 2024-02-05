7a0e3f88abf4a8d2664ed2984cb39d11376d9e4e
Bethânia Alves de Oliveira e Freitas <bethaniafreitas@yahoo.com.br>
2024-02-05 11:24:07 -0300

    seeds e migration para criar produtos no banco

---
+++ GeekShopping/GeekShopping.ProductAPI/Migrations/20240205142051_SeedProductDataTable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProductDataTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[,]
+                {
+                    { 2L, "T-shirt", "Description", "https://github.com/Bethania-Freitas/erudio-microsservices/blob/main/ShoppingImages/11_mars.jpg", "Camiseta_OccupeMars", "69,9" },
+                    { 3L, "Tools", "Description", "https://github.com/Bethania-Freitas/erudio-microsservices/blob/main/ShoppingImages/10_milennium_falcon.jpg?raw=true", "Millenium Falcom", "469,9" },
+                    { 4L, "T-shirt", "Description", "https://github.com/Bethania-Freitas/erudio-microsservices/blob/main/ShoppingImages/12_gnu_linux.jpg?raw=true", "Camiseta Gnu Linux", "69,9" },
+                    { 5L, "T-shirt", "Description", "https://github.com/Bethania-Freitas/erudio-microsservices/blob/main/ShoppingImages/13_dragon_ball.jpg?raw=true", "Camiseta Dragon Ball", "59,9" },
+                    { 6L, "Tools", "Description", "https://github.com/Bethania-Freitas/erudio-microsservices/blob/main/ShoppingImages/14_patch_nasa.jpg?raw=true", "Patch Nasa", "29,9" },
+                    { 7L, "Tools", "Description", "https://github.com/Bethania-Freitas/erudio-microsservices/blob/main/ShoppingImages/1_super_mario.jpg?raw=true", "Caneca Mario", "29,9" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 6L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 7L);
+        }
+    }
+}
--- GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -8,5 +8,68 @@ namespace GeekShopping.ProductAPI.Model.Context
 
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
         public DbSet<Product> Products { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 2,
+                Name = "Camiseta_OccupeMars",
+                Price = new decimal(69.9).ToString(),
+                Description = "Description",
+                ImageURL = "https://github.com/Bethania-Freitas/erudio-microsservices/blob/main/ShoppingImages/11_mars.jpg",
+                CategoryName = "T-shirt",
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 3,
+                Name = "Millenium Falcom",
+                Price = new decimal(469.9).ToString(),
+                Description = "Description",
+                ImageURL = "https://github.com/Bethania-Freitas/erudio-microsservices/blob/main/ShoppingImages/10_milennium_falcon.jpg?raw=true",
+                CategoryName = "Tools",
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 4,
+                Name = "Camiseta Gnu Linux",
+                Price = new decimal(69.9).ToString(),
+                Description = "Description",
+                ImageURL = "https://github.com/Bethania-Freitas/erudio-microsservices/blob/main/ShoppingImages/12_gnu_linux.jpg?raw=true",
+                CategoryName = "T-shirt",
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 5,
+                Name = "Camiseta Dragon Ball",
+                Price = new decimal(59.9).ToString(),
+                Description = "Description",
+                ImageURL = "https://github.com/Bethania-Freitas/erudio-microsservices/blob/main/ShoppingImages/13_dragon_ball.jpg?raw=true",
+                CategoryName = "T-shirt",
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 6,
+                Name = "Patch Nasa",
+                Price = new decimal(29.9).ToString(),
+                Description = "Description",
+                ImageURL = "https://github.com/Bethania-Freitas/erudio-microsservices/blob/main/ShoppingImages/14_patch_nasa.jpg?raw=true",
+                CategoryName = "Tools",
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 7,
+                Name = "Caneca Mario",
+                Price = new decimal(29.9).ToString(),
+                Description = "Description",
+                ImageURL = "https://github.com/Bethania-Freitas/erudio-microsservices/blob/main/ShoppingImages/1_super_mario.jpg?raw=true",
+                CategoryName = "Tools",
+            });
+        }
     }
 }
