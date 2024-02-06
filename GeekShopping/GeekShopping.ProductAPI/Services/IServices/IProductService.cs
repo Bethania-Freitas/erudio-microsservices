e8b7ca585ad61666a8be25b0da4f6909edecc2b4
Bethânia Alves de Oliveira e Freitas <bethaniafreitas@yahoo.com.br>
2024-02-06 09:28:39 -0300

    adicionando o service de produtos e o httpClientExtensions

---
+++ GeekShopping/GeekShopping.ProductAPI/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.ProductAPI.Model;
+
+namespace GeekShopping.ProductAPI.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindProductById(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task<bool> DeleteProductById(long id);
+    }
+}
---
+++ GeekShopping/GeekShopping.ProductAPI/Utils/HttpClientExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace GeekShopping.ProductAPI.Utils
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> HeadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+                throw new ApplicationException($"Something went wrong calling the api" +
+                    $"{response.ReasonPhrase}");
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<T>(
+                dataAsString,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
