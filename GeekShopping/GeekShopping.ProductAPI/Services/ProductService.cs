e7f31a2a542f640801953b77f84abfedf0158eb5
Bethânia Alves de Oliveira e Freitas <bethaniafreitas@yahoo.com.br>
2024-02-06 14:14:06 -0300

    ProductService

---
+++ GeekShopping/GeekShopping.ProductAPI/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using GeekShopping.ProductAPI.Model;
+using GeekShopping.ProductAPI.Services.IServices;
+using GeekShopping.ProductAPI.Utils;
+
+namespace GeekShopping.ProductAPI.Services
+{
+
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _client;
+        public const string BasePath = "api/v1/product";
+
+        public async Task<IEnumerable<ProductModel>> FindAllProducts()
+        {
+            var response = await _client.GetAsync(BasePath);
+            return await response.HeadContentAs<List<ProductModel>>();
+        }
+
+        public async Task<ProductModel> FindProductById(long id)
+        {
+            var response = await _client.GetAsync($"{BasePath}/{id}");
+            return await response.HeadContentAs<ProductModel>();
+        }
+
+        public async Task<ProductModel> CreateProduct(ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ProductModel> UpdateProduct(ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProductById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
