98b626d386f2a27ade27d8326f24a484be9d4335
Bethânia Alves de Oliveira e Freitas <bethaniafreitas@yahoo.com.br>
2024-02-14 15:29:17 -0300

    ajustando layout

--- GeekShopping/Geekhopping.Web/Controllers/HomeController.cs
+++ GeekShopping/Geekhopping.Web/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Geekhopping.Web.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View("~/Views/Home/Index.cshtml");
         }
 
         public IActionResult Privacy()
